c2ecfc10168aa39dd904c646d959b36d0d35a225
Balwinder Katoch <balwinderkatoch@MacBook-Pro.local>
2019-06-20 09:30:12 -0400

    changes

--- gui/src/app/ext-replay-subject-tester/ext-replay-subject-tester.component.ts
+++ gui/src/app/ext-replay-subject-tester/ext-replay-subject-tester.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  ExtendedReplaySubject,
+  ItemComparer,
+  VersionFilterFunc
+} from "../utils/replay-map-subject";
+
+interface Emp {
+  id: number;
+  name: string;
+  v: string;
+}
+const empComparer: ItemComparer<Emp> = (existing: Emp, current: Emp) =>
+  existing.id === current.id;
+const versionComparer: VersionFilterFunc<Emp> = (existing: Emp, current: Emp) =>
+  existing.v === current.v;
 
 @Component({
-  selector: 'app-ext-replay-subject-tester',
-  templateUrl: './ext-replay-subject-tester.component.html',
-  styleUrls: ['./ext-replay-subject-tester.component.scss']
+  selector: "app-ext-replay-subject-tester",
+  templateUrl: "./ext-replay-subject-tester.component.html",
+  styleUrls: ["./ext-replay-subject-tester.component.scss"]
 })
 export class ExtReplaySubjectTesterComponent implements OnInit {
-
-  constructor() { }
+  private xreplayMap = new ExtendedReplaySubject<Emp>(
+    empComparer,
+    undefined,
+    10
+  );
+  private emps: Emp[] = [];
+  constructor() {}
 
   ngOnInit() {
+    this.xreplayMap.subscribe(e => {
+      console.log(`New Emp Received : ${JSON.stringify(e)}`);
+    });
+    this.xreplayMap.deleted$.subscribe(emps => {
+      this.emps = this.emps.filter(em => !emps.some(e => e.id === em.id));
+      console.log(
+        `Employee deleted : ${JSON.stringify(emps)}, Length : ${
+          this.emps.length
+        }`
+      );
+    });
   }
-
+  start() {
+    const emp = this.newEmp();
+    this.emps.push(emp);
+    this.xreplayMap.next(emp);
+  }
+  delete() {
+    const idx = Math.floor(Math.random() * this.emps.length);
+    const emp = this.emps[idx];
+    this.xreplayMap.remove(emp);
+  }
+  duplicate() {
+    debugger;
+    const idx = Math.floor(Math.random() * this.emps.length);
+    const emp = this.emps[idx];
+    this.xreplayMap.next(Object.assign({}, emp));
+  }
+  private newEmp(): Emp {
+    const id = this.randomNum();
+    return {
+      id,
+      name: `Emp ${id}`,
+      v: id.toString()
+    };
+  }
+  private randomNum(): number {
+    return Math.floor(Math.random() * 1e5);
+  }
 }
--- gui/src/app/utils/replay-map-subject.ts
+++ gui/src/app/utils/replay-map-subject.ts
@@ -1,46 +1,69 @@
-import { ReplaySubject, SchedulerLike, Subject } from 'rxjs';
+import { ReplaySubject, SchedulerLike, Subject } from "rxjs";
 
 export type ItemComparer<T> = (existingItem: T, newItem: T) => boolean;
 export type VersionFilterFunc<T> = (prevItem: T, newItem: T) => boolean;
 
 export class ExtendedReplaySubject<T> extends ReplaySubject<T> {
-    private deletedNotifier = new Subject<T[]>();
-    public deleted$ = this.deletedNotifier.asObservable();
-
-    constructor(private comparer: ItemComparer<T>, private versionFilter: VersionFilterFunc<T>,
-        bufferSize?: number, windowTime?: number, scheduler?: SchedulerLike) {
-        super(bufferSize, windowTime, scheduler);
+  private deletedNotifier = new Subject<T[]>();
+  public deleted$ = this.deletedNotifier.asObservable();
+  private superNext: Function;
+  private events: T[];
+  constructor(
+    private comparer: ItemComparer<T>,
+    private versionFilter: VersionFilterFunc<T>,
+    bufferSize?: number,
+    windowTime?: number,
+    scheduler?: SchedulerLike
+  ) {
+    super(bufferSize, windowTime, scheduler);
+    this.superNext = super.next;
+    this.next = this.publish;
+    this.events = this["_events"];
+  }
+  private publish(value: T) {
+    let itemToPublish: T;
+    debugger;
+    const index = this.events.findIndex(evt => {
+      return this.comparer(evt, value);
+    });
+    if (index >= 0) {
+      const existingItem = this.events[index];
+      const isVersionSame = this.versionFilter
+        ? this.versionFilter(existingItem, value)
+        : existingItem === value;
+      if (!isVersionSame) {
+        this.events.splice(index, 1);
+        itemToPublish = value;
+      }
+    } else {
+      itemToPublish = value;
     }
-    public next(value: T) {
-        const index = super['_events'].findIndex((evt) => {
-            return this.comparer(evt, value);
-        });
-        if (index >= 0)  {
-            const existingItem = super['_events'][index];
-            const isVersionSame = this.versionFilter ? this.versionFilter(existingItem, value) : existingItem === value;
-            if (!isVersionSame) {
-                super['_events'].splice(index, 1);
-                super.next(value);
-            }
-        } else {
-            super.next(value);
-        }
+    itemToPublish && this.publishNext(itemToPublish);
+  }
+  private publishNext(value: T) {
+    this.events.push(value);
+    if (this.events.length > this["_bufferSize"]) {
+      this.deletedNotifier.next([this.events.shift()]);
     }
-    public remove(item: T) {
-        const index = super['_events'].findIndex((evt) => {
-            return this.comparer(evt, item);
-        });
-        if (index >= 0) {
-            const existingItem = super['_events'][index];
-            super['_events'].splice(index, 1);
-            this.deletedNotifier.observers.length && this.deletedNotifier.next([existingItem]);
-        }
+    this.superNext(value);
+  }
+  public remove(item: T) {
+    const index = this["_events"].findIndex(evt => {
+      return this.comparer(evt, item);
+    });
+    if (index >= 0) {
+      const existingItem = this["_events"][index];
+      this["_events"].splice(index, 1);
+      this.deletedNotifier.observers.length &&
+        this.deletedNotifier.next([existingItem]);
     }
-    removeAll() {
-        if (super['_events'].length) {
-            const shallowClone = [...super['_events']];
-            super['_events'].length = 0;
-            this.deletedNotifier.observers.length && this.deletedNotifier.next(shallowClone);
-        }
+  }
+  public removeAll() {
+    if (this["_events"].length) {
+      const shallowCopy = [...this["_events"]];
+      this["_events"].length = 0;
+      this.deletedNotifier.observers.length &&
+        this.deletedNotifier.next(shallowCopy);
     }
+  }
 }
